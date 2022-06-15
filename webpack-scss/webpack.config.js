bc543714ccc45fcccd6ef0047d7eaac7b752fbec
Roman Rodin <romaro83@gmail.com>
2022-06-15 22:29:58 +0300

    first

---
+++ webpack-scss/webpack.config.js
@@ -0,0 +1,42 @@
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = "/home/romaro/webpack/";
+const srcPath = __dirname + "webpack-scss/src";
+const dstPath = __dirname + "webpack-scss/dst";
+
+export default {
+  resolve: {
+    modules: ["/home/romaro/webpack/node_modules", "node_modules"],
+  },
+  context: srcPath,
+  entry: "./entry.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: dstPath,
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.woff(2)?$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+};
