79bb4f7ba0782518bdd6ce2806361ac59f33ca98
Roman Rodin <romaro83@gmail.com>
2022-06-24 00:34:38 +0300

    hot reload

--- webpack-dev-server-hot/webpack.config.js
+++ webpack-dev-server-hot/webpack.config.js
@@ -1,3 +1,8 @@
+/**
+ * Возможность горячего обновления поддерживается из коробки.
+ * По этой причине можно не вносить в конфигурацию.
+ */
+
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
@@ -43,11 +48,36 @@ export default {
     },
     plugins: [new CleanWebpackPlugin(), ...getPagePlugins(pageList)],
     devServer: {
+        /**
+         * Видимо, если явно не задано в watchFiles, то файлы будут
+         * отслеживаться в этой директории/директориях
+         * https://webpack.js.org/configuration/dev-server/#devserverstatic
+         */
         static: {
             directory: srcPath,
         },
         compress: true,
         port: 9000,
+        /**
+         * Включает горячее обновление модулей (Hot Module Replacement).
+         * Не следует путать с обновлением страницы, которое управляется
+         * через liveReload. При значении 'only', горячая замена модулей
+         * будет работать, но обновление страницы не будет происходить.
+         * Вроде как аналогично тому, чтобы поставить true
+         * и liveReload: false
+         */
         hot: true,
+        /**
+         * При помощи этой опции можно ограничить файлы, которые
+         * будут отслеживаться. Видимо, по умолчанию отслеживаются файлы,
+         * которые лежат в директории, указанной в devServer.static
+         */
+        watchFiles: ['src/**/*.tsx', 'dist/**/*'],
+        /**
+         * Каким-то хитрым образом взаимодействует с hot, но при отключении
+         * страница обновляться не будет. Вроде как аналогично hot: false
+         * или hot: 'only'
+         */
+        liveReload: true,
     },
 };
